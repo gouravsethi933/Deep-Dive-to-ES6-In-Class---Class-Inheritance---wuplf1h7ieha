44c6eb12f3a8084c8ffefde2d5f6d325ac679650
Gourav Sethi <61932647+gouravsethi933@users.noreply.github.com>
2022-01-21 17:21:13 +0530

    string

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,12 @@
 // declare class
 
+class Shape {
+  constructor(color) {
+    this.color = "blue";
+  }
+  calculateArea() {}
+  drawShape() {}
+}
+
+module.exports = Shape;
 // export class using module.exports
